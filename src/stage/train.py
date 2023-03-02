7772e62d19a2659a4651d49c0b1b06071f337e43
Boru Yang <boruyangtw@gmail.com>
2023-03-02 15:17:52 +0800

    Add train stage

---
+++ src/stage/train.py
@@ -0,0 +1,37 @@
+import argparse
+import numpy as np
+import os
+import torch
+import yaml
+
+from src.utils.logs import get_logger
+from src.utils.train import COVID19Dataset, Model, model_training, same_seed
+from torch.utils.data import DataLoader
+
+def main(config_path):
+    
+    with open(config_path) as conf_file:
+        config = yaml.safe_load(conf_file)
+
+    logger = get_logger(log_level=config['base']['log_level'])
+    file_path = 'data/processed'
+    x_train = np.load(os.path.join(file_path, 'x_train.npy'))
+    y_train = np.load(os.path.join(file_path, 'y_train.npy'))
+    x_valid = np.load(os.path.join(file_path, 'x_valid.npy'))
+    y_valid = np.load(os.path.join(file_path, 'y_valid.npy'))
+    train_dataset, valid_dataset = COVID19Dataset(x_train, y_train), COVID19Dataset(x_valid, y_valid)
+
+    train_loader = DataLoader(train_dataset, batch_size=config['train']['batch_size'], shuffle=True, pin_memory=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=config['train']['batch_size'], shuffle=True, pin_memory=True)
+    
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    same_seed(config['base']['random_state'])
+    model = Model(input_dim=x_train.shape[1]).to(device)
+    model_training(train_loader, valid_loader, model, config, device, logger)
+
+if __name__ == "__main__":
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--config', required=True)
+    args = args_parser.parse_args()
+
+    main(config_path=args.config)
---
+++ src/utils/train.py
@@ -0,0 +1,107 @@
+import math
+import numpy as np
+import os
+import torch
+import torch.nn as nn
+
+from torch.utils.data import Dataset
+from tqdm import tqdm
+
+class COVID19Dataset(Dataset):
+    def __init__(self, x, y=None):
+        if y is None:
+            self.y = y
+        else:
+            self.y = torch.FloatTensor(y)
+        self.x = torch.FloatTensor(x)
+
+    def __len__(self):
+        return len(self.x)
+    
+    def __getitem__(self, idx):
+        if self.y is None:
+            return self.x[idx]
+        else:
+            return self.x[idx], self.y[idx]
+
+def same_seed(seed: int): 
+    '''Fixes random number generator seeds for reproducibility.'''
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
+class Model(nn.Module):
+    def __init__(self, input_dim):
+        super(Model, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, 16),
+            nn.ReLU(),
+            nn.Linear(16, 8),
+            nn.ReLU(),
+            nn.Linear(8, 1)
+        )
+
+    def forward(self, x):
+        x = self.layers(x)
+        x = x.squeeze(1)
+        return x
+
+def model_training(train_loader, valid_loader, model, config, device, logger):
+    '''Model training process'''
+
+    criterion = nn.MSELoss(reduction='mean')
+    optimizer = torch.optim.SGD(model.parameters(), lr=config['train']['learning_rate'], momentum=0.7)
+
+    if not os.path.isdir('./model'):
+        os.mkdir('./model')
+    
+    n_epochs, best_loss, early_stop_count = config['train']['n_epochs'], math.inf, 0
+
+    for epoch in range(n_epochs):
+        model.train()
+        loss_record = []
+
+        train_pbar = tqdm(train_loader, position=0, leave=True)
+
+        for x, y in train_pbar:
+            optimizer.zero_grad()
+            x, y = x.to(device), y.to(device)
+            pred = model(x)
+            loss = criterion(pred, y)
+            loss.backward()
+            optimizer.step()
+            loss_record.append(loss.detach().item())
+
+            train_pbar.set_description(f'Epoch [{epoch + 1} / {n_epochs}]')
+            train_pbar.set_postfix({'loss': loss.detach().item()})
+
+        mean_train_loss = sum(loss_record) / len(loss_record)
+
+        model.eval()
+        loss_record = []
+
+        for x, y in valid_loader:
+            x, y = x.to(device), y.to(device)
+            with torch.no_grad():
+                pred = model(x)
+                loss = criterion(pred, y)
+
+            loss_record.append(loss.item())
+        
+        mean_valid_loss = sum(loss_record) / len(loss_record)
+        logger.info(f'Epoch [{epoch + 1} / {n_epochs}]: Train loss: {mean_train_loss:.4f}, Valid loss: {mean_valid_loss:.4f}')
+
+        if mean_valid_loss < best_loss:
+            best_loss = mean_valid_loss
+            torch.save(model.state_dict(), config['train']['save_path'])
+            logger.info('Saving model with loss {:.3f}...'.format(best_loss))
+            early_stop_count = 0
+        else: 
+            early_stop_count += 1
+
+        if early_stop_count >= config['train']['early_stop']:
+            logger.info('\nModel is not improving, so we halt the training session.')
+            return
